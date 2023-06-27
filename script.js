ab07c1cba16dd8c41ae5df6264bea3354a7c086c
Your Name <you@example.com>
2023-06-26 20:30:05 -0700

    This is my Light Bulb Challenge

---
+++ script.js
@@ -0,0 +1,6 @@
+function btn_On() {
+    document.getElementById('bulb').src='https://clipartcraft.com/images/light-bulb-clipart-royalty-free-2.png';
+}
+function btn_Off() {
+    document.getElementById('bulb').src='https://th.bing.com/th/id/R.425cf8f13a04d489ac06b39434b7f7e9?rik=t7jO%2b%2fS4FE0Qqg&riu=http%3a%2f%2fwww.clker.com%2fcliparts%2fI%2fs%2fH%2fl%2ft%2f7%2foff-lightbulb-hi.png&ehk=Egwrk2XLe9XxysW6MHEsvcTeEf36lu6DzgMkdPfOH7c%3d&risl=&pid=ImgRaw&r=0';
+}
